9ad1016cfb4b4551e41a47a48b0403cdf76335bf
dilraj.vyas <dilraj.vyas@D471.blr.velankani.com>
2017-11-14 17:53:46 +0530

    device repo

--- maven-springboot-database/src/main/java/com/repository/RepositoryService.java
+++
@@ -1,9 +0,0 @@
-package com.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.to.Device;
-
-public interface RepositoryService extends CrudRepository<Device, String> {
-
-}
