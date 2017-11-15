c595bac3c63520770b09594cec674eaecaa6777a
dilraj.vyas <dilraj.vyas@D471.blr.velankani.com>
2017-11-15 12:17:51 +0530

    jpa Connfiguration

--- maven-springboot-database/src/main/java/com/boot/JPAConfiguration.java
+++ maven-springboot-database/src/main/java/com/boot/JPAConfiguration.java
@@ -20,62 +20,68 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class JPAConfiguration {
 
 	@Autowired
-    private Environment environment;
- 
-    /*
-     * Populate SpringBoot DataSourceProperties object directly from application.yml 
-     * based on prefix.Thanks to .yml, Hierachical data is mapped out of the box with matching-name
-     * properties of DataSourceProperties object].
-     */
-  
-	
-	
+	private Environment environment;
+
 	/*
-     * Configure HikariCP pooled DataSource.
-     */
-    @Bean
-    public DataSource dataSource() {
-        
-    	 DriverManagerDataSource dataSource = new DriverManagerDataSource();
-         dataSource.setUrl(environment.getRequiredProperty("spring.datasource.url"));
-         dataSource.setUsername(environment.getRequiredProperty("spring.datasource.username"));
-         dataSource.setPassword(environment.getRequiredProperty("spring.datasource.password"));
-         return dataSource;
-    }
- 
-    /*
-     * Entity Manager Factory setup.
-     */
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() throws NamingException {
-        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
-        factoryBean.setDataSource(dataSource());
-        factoryBean.setPackagesToScan(new String[] { "com.model" });
-        factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
-//        factoryBean.setJpaProperties(jpaProperties());
-        return factoryBean;
-    }
- 
-    /*
-     * Provider specific adapter.
-     */
-    @Bean
-    public JpaVendorAdapter jpaVendorAdapter() {
-        HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
-        return hibernateJpaVendorAdapter;
-    }
- 
-    /*
-     * Here you can specify any provider specific properties.
-     */
-   /* private Properties jpaProperties() {
-        Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("datasource.sampleapp.hibernate.dialect"));
-        properties.put("hibernate.hbm2ddl.auto", environment.getRequiredProperty("spring.jpa.hibernate.ddl-auto"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("datasource.sampleapp.hibernate.show_sql"));
-        properties.put("hibernate.format_sql", environment.getRequiredProperty("datasource.sampleapp.hibernate.format_sql"));
-        
-        return properties;
-    }*/
- 
+	 * Populate SpringBoot DataSourceProperties object directly from
+	 * application.yml based on prefix.Thanks to .yml, Hierachical data is
+	 * mapped out of the box with matching-name properties of
+	 * DataSourceProperties object].
+	 */
+
+	@Bean
+	public DataSource dataSource() {
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setUrl(environment
+				.getRequiredProperty("spring.datasource.url"));
+		dataSource.setUsername(environment
+				.getRequiredProperty("spring.datasource.username"));
+		dataSource.setPassword(environment
+				.getRequiredProperty("spring.datasource.password"));
+		return dataSource;
+	}
+
+	/*
+	 * Entity Manager Factory setup.
+	 */
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory()
+			throws NamingException {
+		LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+		factoryBean.setDataSource(dataSource());
+		factoryBean.setPackagesToScan(new String[] { "com.model" });
+		factoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+		// factoryBean.setJpaProperties(jpaProperties());
+		return factoryBean;
+	}
+
+	/*
+	 * Provider specific adapter.
+	 */
+	@Bean
+	public JpaVendorAdapter jpaVendorAdapter() {
+		HibernateJpaVendorAdapter hibernateJpaVendorAdapter = new HibernateJpaVendorAdapter();
+		return hibernateJpaVendorAdapter;
+	}
+
+	/*
+	 * Here you can specify any provider specific properties.
+	 */
+	/*
+	 * private Properties jpaProperties() { Properties properties = new
+	 * Properties(); properties.put("hibernate.dialect",
+	 * environment.getRequiredProperty
+	 * ("datasource.sampleapp.hibernate.dialect"));
+	 * properties.put("hibernate.hbm2ddl.auto",
+	 * environment.getRequiredProperty("spring.jpa.hibernate.ddl-auto"));
+	 * properties.put("hibernate.show_sql",
+	 * environment.getRequiredProperty("datasource.sampleapp.hibernate.show_sql"
+	 * )); properties.put("hibernate.format_sql",
+	 * environment.getRequiredProperty
+	 * ("datasource.sampleapp.hibernate.format_sql"));
+	 * 
+	 * return properties; }
+	 */
+
 }
